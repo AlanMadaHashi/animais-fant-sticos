5942c07dd9e590bacf0af117e32c155ab18c71b9
AlanGonçal <dev.alancarlos@gmail.com>
2022-10-11 10:38:32 -0300

    add e html css concluido

---
+++ README.md
@@ -0,0 +1,1 @@
+# animais-fant-sticos
---
+++ ex01/script.js
@@ -0,0 +1,85 @@
+// // // // // // // // // function imc(peso,altura){
+// // // // // // // // //     const imc = peso / (altura ** 2);
+// // // // // // // // //     console.log(imc);
+// // // // // // // // // }
+
+// // // // // // // // // imc(80,1.80);
+
+// // // // // // // // // console.log(imc(80,1.80));
+
+// // // // // // // // function terceiraIdade(idade){
+// // // // // // // //  if (typeof idade!== 'number') {
+// // // // // // // //     return 'Por favor preecha um número';
+// // // // // // // //  }else if (idade >= 60) {
+// // // // // // // //     return true;
+// // // // // // // //  } else {
+// // // // // // // //     return false;
+// // // // // // // //  }
+// // // // // // // // }
+// // // // // // // // console.log(terceiraIdade("oi"));
+
+// // // // // // // function faltasVisitados(paisesVisitados){
+// // // // // // //  var totalPaises = 193;
+// // // // // // //  return `Falta visita ${totalPaises - paisesVisitados} países`;
+// // // // // // // }
+// // // // // // // console.log(faltasVisitados(20));
+
+// // // // // // var profissao = 'Designer';
+
+// // // // // // function dados(){
+// // // // // //     var nome = 'Alan';
+// // // // // //  var idade = 28;
+// // // // // //  function outrosDados(){
+// // // // // //     var endereco = 'Margarida';
+// // // // // //     var idade = 25;
+// // // // // //     return`${nome}, ${idade}, ${endereco}, ${profissao} `;
+// // // // // //  }
+// // // // // //  return outrosDados();
+// // // // // // }
+// // // // // // console.log(dados());
+// // // // // // outrosDados();
+// // // // // // function isTruthy(dado){
+// // // // // // return !!dado;
+// // // // // // }   
+
+// // // // // // console.log(isTruthy(25));
+
+
+// // // // // function nomeCompleto(nome,completo,teste){
+// // // // //     teste = 'testando';
+// // // // //     return `${nome}  ${completo} ${teste}`;
+// // // // // }
+// // // // // console.log(nomeCompleto('Alan','Gonçalves'));
+
+
+// // // // function par(numero){
+// // // //     numero = 5;
+// // // //     var repos =  numero % 2;
+   
+// // // // if (repos===0) {
+// // // //    return console.log('Este número é par');
+// // // // } else {
+// // // //    return console.log('Este número não é par');
+    
+// // // // }
+// // // // }
+
+// // // // console.log(par());
+
+// // // function tipoDeDado(dado){
+// // //     dado = true
+// // //     return typeof dado;
+// // // }
+// // // console.log(tipoDeDado());
+
+
+// // function nomeCompletoM(){
+// //     return console.log('Alan Gonçalves');
+// // }
+
+// // addEventListener('scroll', nomeCompletoM);
+
+// function precisoVisitar(paisesVisitados){
+//  var totalPaises = 193;
+//  return ``;
+// }
